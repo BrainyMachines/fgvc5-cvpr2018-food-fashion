db2b0cdfa34aced0a849d6d80b759fb078496f6a
Sourabh Daptardar <saurabh.daptardar@gmail.com>
2018-05-31 10:16:42 +0000

    Fixes for python dependencies

--- jupyter_related.sh
+++ jupyter_related.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+source env.sh
+
+conda install -y jupyter nb_conda
+
 jupyter notebook --generate-config
 jupyter notebook password
+
+conda install -y tqdm ipdb matplotlib
+conda install -y pytorch torchvision cuda91 -c pytorch
+
 
 
