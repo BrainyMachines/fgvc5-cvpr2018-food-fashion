d500a33f0ce1c3c30e89ae00c077d0c4fd5cecfe
Sourabh Daptardar <saurabh.daptardar@gmail.com>
2018-06-11 13:45:17 +0000

    Script to cleanup output directory for restart

---
+++ cleanup_output.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -vx
+
+rm -rf /mnt/disks/imaterialist_fashion/data_ifood/output/submissions
+mkdir /mnt/disks/imaterialist_fashion/data_ifood/output/submissions
+rm -rf /mnt/ram-disk/imaterialist_fashion/data_ifood/output/submissions
+mkdir /mnt/ram-disk/imaterialist_fashion/data_ifood/output/submissions
